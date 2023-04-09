944cd849174fa093a06bd53ef17d5ad93e3b5092
Jean Sidharta <jeansidharta@gmail.com>
2023-04-09 11:19:29 -0300

    Better mouse speed support

--- config.h
+++ config.h
@@ -29,3 +29,23 @@
 #define MASTER_LEFT
 
 #define SERIAL_USART_TX_PIN 16
+
+#define MK_3_SPEED
+
+#define MK_C_OFFSET_0 4
+#define MK_C_INTERVAL_0 16
+
+#define MK_C_OFFSET_1 16
+#define MK_C_INTERVAL_1 16
+
+#define MK_C_OFFSET_2 32
+#define MK_C_INTERVAL_2 16
+
+#define MK_W_OFFSET_0 1
+#define MK_W_INTERVAL_0 120
+
+#define MK_W_OFFSET_1 1
+#define MK_W_INTERVAL_1 120
+
+#define MK_W_OFFSET_2 1
+#define MK_W_INTERVAL_2 120
