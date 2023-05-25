a9be0f86b01baf533f30264c0ee69737811c5605
Jean Sidharta <jeansidharta@gmail.com>
2023-05-25 11:46:57 -0300

    Added alt to gaming profile

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_ESC, KC_Q,    KC_G,             KC_E,    KC_R,    KC_T,   /*|*/ KC_Y,    KC_U,   KC_I,     KC_O,    KC_P,         DF(LAYER_0),
 		KC_TAB, KC_A,    KC_S,             KC_W,    KC_D,    KC_F,   /*|*/ KC_H,    KC_J,   KC_K,     KC_L,    KC_SEMICOLON, KC_QUOTE,
 		KC_DEL, KC_Z,    KC_X,             KC_C,    KC_V,    KC_B,   /*|*/ KC_N,    KC_M,   KC_COMMA, KC_DOT,  KC_SLASH,     KC_GRV,
-		        XXXXXXX, MO(LAYER_GAME_1), KC_LCTL, KC_LSFT, KC_SPC, /*|*/ XXXXXXX, KC_ENT, XXXXXXX,  KC_BSPC, XXXXXXX
+		        XXXXXXX, MO(LAYER_GAME_1), KC_LCTL, KC_LSFT, KC_SPC, /*|*/ KC_LALT, KC_ENT, XXXXXXX,  KC_BSPC, XXXXXXX
 	),
 	[LAYER_GAME_1] = LAYOUT(
 		_______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    /*|*/ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DF(LAYER_0),
